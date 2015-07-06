8089ba2c20fb227b1fede07cd6edf10f0f523eda
Iván Berga <verlak46@gmail.com>
2015-07-06 17:39:54 +0200

    Ejercicio P2P Obligatorio

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,12 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET home page. */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Autor' });
+});
+
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
