0e75315594cff3aec35121a215a0490418f0adf6
Iván Berga <verlak46@gmail.com>
2015-07-15 18:56:03 +0200

    Múltiples preguntas y Autoload

--- models/models.js
+++ models/models.js
@@ -32,14 +32,17 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
 // sequelize.sync() inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
-  // success(..) ejecuta el manejador una vez creada la tabla
+sequelize.sync().then(function() {
+  // then(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.create({ pregunta: 'Capital de Italia',
                     respuesta: 'Roma'
+                  });
+      Quiz.create({ pregunta: 'Capital de Portugal',
+                    respuesta: 'Lisboa'
                   })
-      .success(function(){console.log('Base de datos inicializada')});
+      .then(function(){console.log('Base de datos inicializada')});
     };
   });
 });        
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,17 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-/* GET home page. */
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
+/* GET Author */
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Autor' });
 });
 
-
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Definición de rutas de /quizes
+router.get('/quizes', 					   quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
